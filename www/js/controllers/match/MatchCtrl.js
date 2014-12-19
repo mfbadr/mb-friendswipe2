04ace2cd3874c227d4a7290ebb58e6130815a676
Mikey Badr <mfbadr@gmail.com>
2014-12-19 10:06:11 -0600

    friends data

--- www/js/controllers/match/MatchCtrl.js
+++ www/js/controllers/match/MatchCtrl.js
@@ -2,6 +2,8 @@
   'use strict';
   angular.module('mb-friendswipe')
   .controller('MatchCtrl', ['$scope', 'Friends', function($scope, Friends){
-    $scope.friends = Friends.all();
+    //$scope.friends = Friends.all();
+    //make a call to the database to find all friends who match
+    //
   }]);
 })();
--- www/js/controllers/match/MatchDetailCtrl.js
+++ www/js/controllers/match/MatchDetailCtrl.js
@@ -2,6 +2,7 @@
   'use strict';
   angular.module('mb-friendswipe')
   .controller('MatchDetailCtrl', ['$scope', '$state' , 'Friends', function($scope, $state, Friends){
-    $scope.friend = Friends.get($state.params.friendId);
+    //$scope.friend = Friends.get($state.params.friendId);
+    //get as many details on friendship as possible, link to message from app
   }]);
 })();
