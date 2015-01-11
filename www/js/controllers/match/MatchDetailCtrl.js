93e03a261d5778282b308873076a27dda72da277
Mikey Badr <mfbadr@gmail.com>
2015-01-11 14:47:16 -0600

    friend detail

--- www/js/controllers/match/MatchDetailCtrl.js
+++ www/js/controllers/match/MatchDetailCtrl.js
@@ -3,7 +3,13 @@
   angular.module('mb-friendswipe')
   .controller('MatchDetailCtrl', ['$scope', '$state' , 'OpenFB', function($scope, $state, OpenFB){
 
-    //$scope.friend = Friends.get($state.params.friendId);
-    //get as many details on friendship as possible, link to message from app
+    OpenFB.api({path:'/' + $state.params.friendId + '/picture?redirect=0&type=large'}).then(function(response){
+      $scope.friendPicture = response.data.data.url;
+    });
+
+    OpenFB.api({path:'/' + $state.params.friendId}).then(function(response){
+      $scope.friend = response.data;
+    });
+
   }]);
 })();
