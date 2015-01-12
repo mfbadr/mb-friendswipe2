49c3baefae0dbe2bda3d50279fb5b7e46829dec5
Mikey Badr <mfbadr@gmail.com>
2015-01-11 18:13:07 -0600

    thumbnail pictures on match page

--- www/js/controllers/match/MatchCtrl.js
+++ www/js/controllers/match/MatchCtrl.js
@@ -1,12 +1,28 @@
+/* jshint camelcase:false*/
+
+
 (function(){
   'use strict';
   angular.module('mb-friendswipe')
-  .controller('MatchCtrl', ['$scope', 'Match', '$rootScope', function($scope, Match, $rootScope){
+  .controller('MatchCtrl', ['$scope', 'Match', '$rootScope', 'OpenFB', function($scope, Match, $rootScope, OpenFB){
 
     Match.matches($rootScope.myFacebookId).then(function(response){
       console.log('response from matches', response);
-      $scope.matches = response.data;
+      $scope.matches = friendPictures(response.data);
+
     }, function(response){});
+
+    function friendPictures(matchArray){
+      matchArray = matchArray.map(function(match){
+        var url;
+        OpenFB.api({path:'/' + match.sender_id + '/picture?redirect=0&type=large'}).then(function(data){
+          url = data.data.data.url;
+          match.url = url;
+        });
+        return match;
+      });
+      return matchArray;
+    }
 
   }]);
 })();
